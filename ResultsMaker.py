243c132734fc1e70cd99cb4310b793fdd3461a77
Myles <mmcmanus@thewaterinstitute.org>
2022-10-03 10:15:29 -0500

    updated to use json for gage info and match LWI convention for output file names.

--- ResultsMaker.py
+++ ResultsMaker.py
@@ -7,132 +7,140 @@ import plotly
 from plotly.graph_objs import Scatter, Layout
 from plotly.subplots import make_subplots
 import plotly.graph_objects as go
+import json
 
 
 # gageList = ['Amite', 'Maurepas', 'Tangipahoa', 'Tickfaw']
 
-gageList = [
-    'J_UpperAmite','J_Sandy_AmiteDS','J_at_ComiteDr','J_Amite_Comite',
-    'J_UpperTickfallOut','J_TwelveMiCrkOut','J_TickfallHoldenOutlet','J_UpperNatalOutlet',
-    'J_LittleTangOutlet','J_ChapOutlet'
-    ]
+# gageList = [
+#     'J_UpperAmite','J_Sandy_AmiteDS','J_at_ComiteDr','J_Amite_Comite',
+#     'J_UpperTickfallOut','J_TwelveMiCrkOut','J_TickfallHoldenOutlet','J_UpperNatalOutlet',
+#     'J_LittleTangOutlet','J_ChapOutlet'
+#     ] 
 
+
+with open("gage.json", "r") as read_file:
+    gages = json.load(read_file)
+
+print (gages['gages'].keys())
+print (gages['gages']['J_TickfallHoldenOutlet'])
+# print(list(gages['gages'].keys())[list(gages['gages'][''].values()).index('J_TickfallHoldenOutlet')])
 parameter = 'Flow'
 units = 'cfs'
 
 eventList = ['2004_Matthew', '2005_Rita', '2012_Isaac']
-# eventList = ['2012_Isaac']
 
+# basinList = ACT = Actual AORC calibrated basin, 001 = 25% bin, 003 = 75% bin
+basinList = ['ACT', '001', '002', '003']
 
-        
+#for testing
+basinList = ['ACT']
+eventList = ['2004_Matthew']       
 
 for event in eventList:
-    n_columns = 1
-    n = len(gageList)
-    i=0
-    fig = None
-    # Setup plotly figure
-    fig = make_subplots(  
-        rows=n, 
-        cols=n_columns, 
-        # vertical_spacing=3,
-        # padding = 
-        # vertical_spacing=(1 / (n - 1)),
-        subplot_titles=gageList
-    )
-    # For each dss file, get precip-inc at a location
-    dss_dir = fr"Z:\LWI\EJPM-OS-Param-Rainfall-Tests\hms_out_dss\{event}"
-    dss_files = glob.glob(dss_dir+"//*.dss")
-    dss_files = sorted(dss_files)
-
-    for i, gage in enumerate(gageList):
-        # concatenate dataframes for each sim to a single dataframe that will be output to a csv file
-        df_gage = pd.DataFrame()
-        for dss_file in dss_files:
-            
-            sim = dss_file.split(".")[0].split("_")[-1]
-            fid = HecDss.Open(fr"{dss_file}")
-            pathname_pattern = f'//{gage}/{parameter}/*/1HOUR/*/'
-            path_list = fid.getPathnameList(pathname_pattern,sort=1)
-            subbasinList = []
-            DSSpaths_List = []
-            
-            for path in path_list:
-                
-                pathSplit = path.split("/")
-                subbasin = pathSplit[2]
-                cPart = pathSplit[3]
-                ePart = pathSplit[5]
-                fPart = pathSplit[6]
-                subbasinList.append(subbasin)
-                
-                # Remove d-part
-                noDpartPath = f"//{subbasin}/{cPart}//{ePart}/{fPart}/"
-                DSSpaths_List.append(noDpartPath)
-            
-            # sort the lists alphabetically, and remove dups
-            subbasinList = sorted(list(set(subbasinList)))
-            DSSpaths_List = sorted(list(set(DSSpaths_List)))
-            
-            n = len(DSSpaths_List)
-            subPlotTitleList  = subbasinList
-
-            for path in DSSpaths_List:
-                
-                ts = fid.read_ts(path)
-                
-                # setup dataframe of trace
-                df = pd.DataFrame()
-                df['Times'] = ts.pytimes
-                df['Values'] = ts.values
-                df['Missing'] = ts.nodata
-                df.Values = np.where(df.Missing == True, np.NaN, df.Values)
-                df['sim'] = sim
-                
-                # concatenate dataframes for each sim to a single dataframe that will be output to a csv file
-                df_gage = pd.concat([df_gage,df])
+    for basin in basinList:
+        n_columns = 1
+        n = len(gages['gages'].keys())
+        i=0
         
+        # Setup plotly figure
+        # fig = make_subplots(  
+        #     rows=n, 
+        #     cols=n_columns, 
+        #     # vertical_spacing=3,
+        #     # padding = 
+        #     # vertical_spacing=(1 / (n - 1)),
+        #     subplot_titles=gageList
+        # )
 
-                fig.append_trace(
-                    go.Scatter(
-                        x=df.Times, 
-                        y=df.Values,
-                        name = sim,
-                    ), 
-                    row=i+1,
-                    # row = 1, 
-                    col=1
-                )
+        # For each dss file, get precip-inc at a location
+        dss_dir = fr"Z:\LWI\EJPM-OS-Param-Rainfall-Tests\hms_out_dss\{event}"
+        dss_files = glob.glob(dss_dir+"//*.dss")
+        dss_files = sorted(dss_files)
 
-                
-            
-            fid.close()
+        for i, gage in enumerate(gages['gages'].keys()):
         
-        # concatenate dataframes for each sim to a single dataframe that will be output to a csv file
-        df_gage.to_csv(f'{event}_{gage}.csv')
+            # concatenate dataframes for each sim to a single dataframe that will be output to a csv file
+            df_gage = pd.DataFrame()
+            for dss_file in dss_files:
+                
+                sim = dss_file.split(".")[0].split("_")[-1]
+                fid = HecDss.Open(fr"{dss_file}")
+                pathname_pattern = f'//{gage}/{parameter}/*/1HOUR/*/'
+                path_list = fid.getPathnameList(pathname_pattern,sort=1)
+                subbasinList = []
+                DSSpaths_List = []
+                
+                for path in path_list:
+                    
+                    pathSplit = path.split("/")
+                    subbasin = pathSplit[2]
+                    cPart = pathSplit[3]
+                    ePart = pathSplit[5]
+                    fPart = pathSplit[6]
+                    subbasinList.append(subbasin)
+                    
+                    # Remove d-part
+                    noDpartPath = f"//{subbasin}/{cPart}//{ePart}/{fPart}/"
+                    DSSpaths_List.append(noDpartPath)
+                
+                # sort the lists alphabetically, and remove dups
+                subbasinList = sorted(list(set(subbasinList)))
+                DSSpaths_List = sorted(list(set(DSSpaths_List)))
+                
+                n = len(DSSpaths_List)
+                subPlotTitleList  = subbasinList
 
-        # fig.add_annotation(
-        #             text=gage,
-        #             xref="x domain", yref="y domain",
-        #             x=0.9, y=0.9, showarrow=False,
-        #             row=i+1,
-        #             # row = 1, 
-        #             col=1,
-        #             font=dict(family='sans-serif', size=22)
+                for path in DSSpaths_List:
+                    
+                    ts = fid.read_ts(path)
+                    
+                    # setup dataframe of trace
+                    df = pd.DataFrame()
+                    df['Times'] = ts.pytimes
+                    df['Values'] = ts.values
+                    df['Missing'] = ts.nodata
+                    df.Values = np.where(df.Missing == True, np.NaN, df.Values)
+                    df['sim'] = sim
+                    
+                    # concatenate dataframes for each sim to a single dataframe that will be output to a csv file
+                    df_gage = pd.concat([df_gage,df])
+            
+
+                    # fig.append_trace(
+                    #     go.Scatter(
+                    #         x=df.Times, 
+                    #         y=df.Values,
+                    #         name = sim,
+                    #     ), 
+                    #     row=i+1,
+                    #     # row = 1, 
+                    #     col=1
+                    # )
+
+                    
+                
+                fid.close()
+            
+            # concatenate dataframes for each sim to a single dataframe that will be output to a csv file
+            # CSV Output Format: Ex Q_Matthew_PARA_001_500_G01 --> Parameter_PARA_AntecedentBasin_Sim#_G#
+            g = gages['gages'][gage]['g_name']
+            df_gage.to_csv(f'Q_{event}_PARA_{basin}_{g}.csv')
+
+
+
+        # fig.update_layout(
+        #             height=n*10000, 
+        #             width=2300,
+        #             showlegend=False, 
+        #             title_text=f"JPM Parametric Rainfall Simulations. {event} - {parameter} ({units})",
+        #             template= "plotly_dark",
+        #             hoverlabel=dict(
+        #                 font=dict(
+        #                     family='sans-serif', size=22
+        #                 ),
+        #                 namelength= -1
+        #             )
         #         )
-
-    fig.update_layout(
-                height=n*10000, 
-                width=2300,
-                showlegend=False, 
-                title_text=f"JPM Parametric Rainfall Simulations. {event} - {parameter} ({units})",
-                template= "plotly_dark",
-                hoverlabel=dict(
-                    font=dict(
-                        family='sans-serif', size=22
-                    ),
-                    namelength= -1
-                )
-            )
-    fig.update_yaxes(automargin=True)
-    fig.write_html(f"{event} Results.html")
+        # fig.update_yaxes(automargin=True)
+        # fig.write_html(f"{event} {basin} Results.html")
